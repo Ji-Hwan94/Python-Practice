fc4efd1660ee8e3877353c7cc8aed476ed1286fe
황 지환 <76830553+Ji-Hwan94@users.noreply.github.com>
2021-01-28 22:57:07 +0900

    Create&Read&Update&Delete
    With python, I make the web which can create index, update and delete. I used  very easy but import method like 'for in', 'if else'. And it was related by pageId that get them by 'FieldStorage'.

---
+++ CRUD/process_create.py
@@ -0,0 +1,14 @@
+#!C:\Users\황지환\AppData\Local\Programs\Python\Python39\python.exe
+
+import cgi
+form = cgi.FieldStorage()
+title = form["title"].value
+description = form['desc'].value
+
+opened_file = open('data/'+title, 'w')
+opened_file.write(description)
+opened_file.close()
+
+#Redirection
+print("Location: index.py?id=" + title)
+print()
